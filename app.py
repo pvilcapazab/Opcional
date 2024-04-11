4572c0b15e7ea19b3b38236584a26d82db8a85fb
Paul <129361725+pvilcapazab@users.noreply.github.com>
2024-04-11 23:25:38 +0000

    modified:   app.py 	modified:   templates/index.html

--- app.py
+++ app.py
@@ -3,5 +3,11 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return render_template("index.html", title="Hello")
+def index():
+    personas = [
+        {"nombre": "Juan", "apellidos": "Pérez", "id": "1", "cumpleaños": "10/03/1984", "gastos": 15000},
+        {"nombre": "María", "apellidos": "González", "id": "2", "cumpleaños": "02/07/1976", "gastos": 27500},
+        {"nombre": "Pedro", "apellidos": "Rodríguez", "id": "3", "cumpleaños": "25/11/1995", "gastos": 19200}
+    ]
+    total = sum(persona["gastos"] for persona in personas)
+    return render_template("index.html", personas=personas, total=total)
